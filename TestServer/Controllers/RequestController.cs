3eabe92544b450c4813b9368d78d240eb8f7ccf6
kentxxq <805429509@qq.com>
2023-04-13 22:01:31 +0800

    feat: 完成输出requestInfo

---
+++ TestServer/Controllers/RequestController.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.Encodings.Web;
+using System.Text.Json;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestServer.Controllers;
+
+/// <summary>
+/// request控制器
+/// </summary>
+[ApiExplorerSettings(GroupName = "V1")]
+[ApiController]
+[Route("[controller]")]
+public class RequestController : Controller
+{
+    /// <summary>
+    /// 返回请求的相关信息
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<string> ReturnRequestInfo()
+    {
+        Dictionary<string, object> data = new();
+        
+        var headers = HttpContext.Request.Headers.ToDictionary(h => h.Key, h => h.Value.ToString());
+        if (headers.Count > 0)
+        {
+            data.Add("Headers",headers);
+        }
+        
+        data.Add("method",HttpContext.Request.Method);
+        data.Add("schema",HttpContext.Request.Scheme);
+        data.Add("host",HttpContext.Request.Host);
+        data.Add("path",HttpContext.Request.Path);
+        data.Add("queryString",(string.IsNullOrEmpty(HttpContext.Request.QueryString.Value)?"无查询参数":HttpContext.Request.QueryString.Value));
+
+        using var reader = new StreamReader(HttpContext.Request.Body);
+        var requestBody = await reader.ReadToEndAsync();
+
+        if (!string.IsNullOrEmpty(requestBody))
+        {
+            data.Add("body",requestBody);
+        }
+
+        return JsonSerializer.Serialize(data, new JsonSerializerOptions
+        {
+            WriteIndented = true,
+            Encoder = JavaScriptEncoder.UnsafeRelaxedJsonEscaping
+        });
+    }
+}
