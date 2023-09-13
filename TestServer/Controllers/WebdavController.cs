56bad5157a45890386af2e0982cad6ce957732bb
kentxxq <805429509@qq.com>
2023-09-13 17:31:22 +0800

    feat: 加入webdav文件下载

---
+++ TestServer/Controllers/WebdavController.cs
@@ -0,0 +1,34 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Mvc;
+using TestServer.RO;
+using WebDav;
+
+namespace TestServer.Controllers;
+
+/// <summary>
+///     delay控制器
+/// </summary>
+[ApiExplorerSettings(GroupName = "V1")]
+[ApiController]
+[Route("[controller]")]
+public class WebdavController : ControllerBase
+{
+    /// <summary>
+    ///     获取特定webdav文件
+    /// </summary>
+    /// <param name="webdavInfo">webdav的文件信息</param>
+    /// <returns></returns>
+    [HttpGet]
+    public async Task<IActionResult> GetWebdavFile([FromQuery] WebdavInfo webdavInfo)
+    {
+        var clientParams = new WebDavClientParams
+        {
+            BaseAddress = new Uri(webdavInfo.Server),
+            Credentials = new NetworkCredential(webdavInfo.Username, webdavInfo.Password)
+        };
+        var client = new WebDavClient(clientParams);
+        var stream = await client.GetRawFile(webdavInfo.FilePath);
+        Response.Headers.Add("Content-Disposition", $"attachment; filename={Path.GetFileName(webdavInfo.FilePath)}");
+        return new FileStreamResult(stream.Stream, "application/octet-stream");
+    }
+}
---
+++ TestServer/RO/WebdavInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace TestServer.RO;
+
+public class WebdavInfo
+{
+    public string Server { get; set; }
+    public string Username { get; set; }
+    public string Password { get; set; }
+    public string FilePath { get; set; }
+}
