89c4d262fb670792a2ea13d3fd2e46a5dfbaa0c0
kentxxq <805429509@qq.com>
2024-06-25 16:06:25 +0800

    调试在resource添加ip信息

---
+++ TestServer/Common/StaticNetTool.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.NetworkInformation;
+using System.Net.Sockets;
+
+namespace TestServer.Common;
+
+public static class StaticNetTool
+{
+    /// <summary>
+    /// 获取本机ipv4内网ip<br />
+    /// 如果网络不可用返回127.0.0.1<br />
+    /// 如果之前网络可用，可能会返回之前保留下来的ip地址
+    /// </summary>
+    /// <returns></returns>
+    public static IPAddress GetLocalIP()
+    {
+        try
+        {
+            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
+            socket.Connect("223.5.5.5", 53);
+            var endPoint = socket.LocalEndPoint as IPEndPoint;
+            endPoint ??= new IPEndPoint(IPAddress.Loopback, 0);
+            return endPoint.Address;
+        }
+        catch (Exception)
+        {
+            return IPAddress.Loopback;
+        }
+    }
+}
--- TestServer/Extensions/LogExtensions.cs
+++ TestServer/Extensions/LogExtensions.cs
@@ -9,19 +9,18 @@ namespace TestServer.Extensions;
 
 public static class LogExtensions
 {
-    public static string DefaultLogTemplate =
+    public static readonly string DefaultLogTemplate =
         "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz}|{Level:u3}|{SourceContext}|{IP}|{MachineName}|{ThreadName}|{ThreadId}|{Message:lj}{Exception}{NewLine}";
 
     private static LoggerConfiguration AddCommonConfig(this LoggerConfiguration loggerConfiguration)
     {
-        var ip = GetLocalIP();
         return loggerConfiguration
             .MinimumLevel.Is(LogEventLevel.Information)
             .MinimumLevel.Override("Microsoft.AspNetCore", LogEventLevel.Warning)
             .MinimumLevel.Override("System", LogEventLevel.Warning)
             .Enrich.WithProperty("AppName", ThisAssembly.Project.AssemblyName)
             .Enrich.WithMachineName()
-            .Enrich.WithProperty("IP",ip.ToString())
+            .Enrich.WithProperty("IP",StaticNetTool.GetLocalIP().ToString())
             .Enrich.When(logEvent => !logEvent.Properties.ContainsKey("SourceContext"),
                 enrichmentConfig => enrichmentConfig.WithProperty("SourceContext", "SourceContext"))
             .Enrich.When(logEvent => !logEvent.Properties.ContainsKey("ThreadName"),
@@ -73,30 +72,8 @@ public static class LogExtensions
                                    throw new InvalidOperationException("必须配置open telemetry的collector地址");
                 options.ResourceAttributes["service.name"] = ThisAssembly.Project.AssemblyName;
                 options.ResourceAttributes["job"] = ThisAssembly.Project.AssemblyName;
+                options.ResourceAttributes["server.ip"] = StaticNetTool.GetLocalIP().ToString();
             }
         );
-    }
-
-
-    /// <summary>
-    /// 获取本机ipv4内网ip<br />
-    /// 如果网络不可用返回127.0.0.1<br />
-    /// 如果之前网络可用，可能会返回之前保留下来的ip地址
-    /// </summary>
-    /// <returns></returns>
-    public static IPAddress GetLocalIP()
-    {
-        try
-        {
-            using var socket = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, 0);
-            socket.Connect("223.5.5.5", 53);
-            var endPoint = socket.LocalEndPoint as IPEndPoint;
-            endPoint ??= new IPEndPoint(IPAddress.Loopback, 0);
-            return endPoint.Address;
-        }
-        catch (Exception)
-        {
-            return IPAddress.Loopback;
-        }
     }
 }
